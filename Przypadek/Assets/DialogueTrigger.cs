0398470b5e42523ed4034f00612b492bd0e1282d
Karol Wojtachnia <50627253+KarolWojtachnia@users.noreply.github.com>
2021-04-18 20:08:56 +0200

    Kloizje i dialogi
    Postać zatrzymuje się na dużych kamieniach i drzewach.
    Po podejściu do tabliczki można wcisnąć 'e' i przejść do dialogu.

---
+++ Przypadek/Assets/DialogueTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    private GameObject triggeringNpc;
+    private bool triggering;
+
+    void Update()
+    {
+        if (triggering){
+
+        }
+
+        if (Input.GetButtonDown("Interact") && triggeringNpc){
+            DialogueManager.Instance.StartDialogue(dialogue);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        
+        if (other.tag == "Player"){
+            triggering = true;
+            triggeringNpc = other.gameObject;
+            Debug.Log("Triggered by "+triggeringNpc);            
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other){
+        if (other.tag == "Player"){
+            Debug.Log("Not triggering with "+triggeringNpc);            
+            triggering = false;
+            triggeringNpc = null;
+        }
+    }
+
+}
