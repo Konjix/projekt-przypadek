9251f7dbdfb5ce08f94f77032a0a55af3f69e616
Konjix <Konjix@users.noreply.github.com>
2021-04-26 02:19:15 +0200

    Player prefab and organizing
    -Added fully animated player prefab (and switched it in Sample Scene)
    -Changed NPC's prefabs' colliders (with one being trigger)
    -Organized project's assets and game objects in Sample Scene

---
+++ Przypadek/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Animator animator;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 currentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+
+        float newVelocityX = 0f;
+
+        if (moveHorizontal < 0 && currentVelocity.x <= 0)
+        {
+            newVelocityX = -speed;
+            animator.SetBool("walkingLeft", true);
+        }
+        else if (moveHorizontal > 0 && currentVelocity.x >= 0)
+        {
+            newVelocityX = speed;
+            animator.SetBool("walkingRight", true);
+        }
+        else
+        {
+            animator.SetBool("walkingLeft", false);
+            animator.SetBool("walkingRight", false);
+        }
+
+        float newVelocityY = 0f;
+
+        if (moveVertical < 0 && currentVelocity.y <= 0)
+        {
+            newVelocityY = -speed;
+            animator.SetBool("walkingDown", true);
+        }
+        else if (moveVertical > 0 && currentVelocity.y >= 0)
+        {
+            newVelocityY = speed;
+            animator.SetBool("walkingUp", true);
+        }
+        else
+        {
+            animator.SetBool("walkingDown", false);
+            animator.SetBool("walkingUp", false);
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(newVelocityX, newVelocityY);
+    }
+}
+
